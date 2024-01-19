5fcf169cd3fb2dc1f62580c809d1220369045aa7
georgemiraclee <georgemiracle2019@gmail.com>
2024-01-19 16:39:10 +0700

    jasa supir

--- src/app/api/service/route.js
+++ src/app/api/service/route.js
@@ -18,18 +18,13 @@ export async function GET(req, res) {
     },
     {
       id:2,
-      title:'Jasa Pemasangan Plafon PVC',
+      title:'Jasa Supir Mobil Semarang',
       description: `
-      <p>Kami telah meyelesaikan proyek plafon PVC seperti plafon Masjid, proyek rumah tinggal, serta berbagai proyek lainnya </p>
-      <p>Pemasangan plafon PVC sesuai lengkap mulai dari rangka plafon, plafon PVC, lis profil, ornamen hingga lampu Strip. Kami mampu mengerjakan proyek plafon PVC berbagai model dan variasi dengan jaminan kualitas pemasangan dan keamanan.</p>
-      <p>Dengan harga mulai dari 150rb/m Anda sudah dapat plafon PVC komplit dengan rangka dan lis profil. Anda dapat menyesuaikan kebutukan plafon dengan kami. Mari konsultasikan kebutuhanmu gratis</p>
+      <p>Jasa supir mobil lintas Semarang yang andal untuk perjalanan aman dan nyaman. Dengan tim berpengalaman dan armada terawat, kami siap membawa Anda ke tujuan tepat waktu. Percayakan perjalanan Anda kepada kami untuk pengalaman berkendara tanpa khawatir dan memuaskan.</p>
       `,
       imageUrl:[
-        'https://res.cloudinary.com/ddjmxrrij/image/upload/v1700890301/sga/products/mn0kgdreqf2p0rncbdew.jpg',
-        'https://res.cloudinary.com/ddjmxrrij/image/upload/v1700889222/sga/products/ktphf0nay0pg2qpe9j7o.jpg',
-        'https://res.cloudinary.com/ddjmxrrij/image/upload/v1700889226/sga/products/ej3x0x2khf0k638hgt0o.jpg',
-        'https://res.cloudinary.com/ddjmxrrij/image/upload/v1700889825/sga/products/mrmxqsh0csc7i0ivy92y.jpg',
-        'https://res.cloudinary.com/ddjmxrrij/image/upload/v1700890365/sga/products/v17jqscqjoki9pm6yvdi.jpg',
+        'https://res.cloudinary.com/dgcedsrzf/image/upload/v1705656873/sewa-mobil-di-jakarta_xznpjt.png',
+        'https://res.cloudinary.com/dgcedsrzf/image/upload/v1705657054/sewa-xenia-jogja_aojzvm.png',
       ]
     },
   ])
